6f65cd231280877a3aa2ef3d314fb49770ad2dc0
kirsanovmike <kirsanovmike@gmail.com>
2023-12-06 22:18:14 +0300

    initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="home">
-    
-    <h2>{{ appTitle }}</h2>
+
+    <h2>{{ mainTitle }}</h2>
 
     <h3>{{ counterData.title }}:</h3>
 
     <div>
-      <button @click="decreaseCounter(2)" class="btn">--</button>
-      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <button class="btn" @click="decreaseCounter(2)">--</button>
+      <button class="btn" @click="decreaseCounter(1)">-</button>
       <span class="counter">{{ counterData.count }}</span>
-      <button @click="increaseCounter(1, $event)" class="btn">+</button>
-      <button @click="increaseCounter(2)" class="btn">++</button>
+      <button class="btn" @click="increaseCounter(1, $event)">+</button>
+      <button class="btn" @click="increaseCounter(2)">++</button>
     </div>
 
     <p>This counter is {{ oddOrEven }}</p>
+    <h1>
+      {{ tmp }}
+    </h1>
 
     <div class="edit">
       <h4>Edit counter title:</h4>
-      <input v-model="counterData.title" type="text" v-autofocus>
+      <input v-model="counterData.title" v-autofocus type="text">
     </div>
 
   </div>
@@ -28,98 +31,77 @@
   imports
 */
 
-  import { reactive, computed, watch, onMounted } from 'vue'
-  import { vAutofocus } from '@/directives/vAutofocus'
+import {computed, onMounted, reactive, ref, watch} from 'vue'
+import {vAutofocus} from '@/directives/vAutofocus'
 
 /*
   app title
 */
 
-  const appTitle = 'My Ok Counter App'
+const mainTitle = "Super Calculator"
 
-  onMounted(() => {
-    console.log('Do stuff related to App Title')
-  })
+onMounted(() => {
+  console.log("do some stuff on mounted hook")
+})
 
 /*
   counter
 */
 
-  const counterData = reactive({
-    count: 0,
-    title: 'My Counter'
-  })
+const counterData = reactive({
+  count: 0,
+  title: "counterData_title"
+})
 
-  watch(() => counterData.count, (newCount) => {
-    if (newCount === 20) {
-      alert('Way to go! You made it to 20!!')
-    }
-  })
-
-  const oddOrEven = computed(() => {
-    if (counterData.count % 2 === 0) return 'even'
-    return 'odd'
-  })
-
-  const increaseCounter = (amount, e) => {
-    counterData.count += amount
+watch(() => counterData.count, (newCount) => {
+  tmpCom.value = newCount
+  if (newCount === 20) {
+    alert('Way to go! You made it to 20!!')
   }
+})
 
-  const decreaseCounter = amount => {
-    counterData.count -= amount
+let tmp = ref(0)
+
+let tmpCom = computed({
+  get() {
+    return counterData.count * 3;
+  },
+  set(newValue) {
+    tmp.value = newValue - 1
   }
+})
 
-  onMounted(() => {
-    console.log('Do stuff related to Counter')
-  })
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return 'even'
+  return 'odd'
+})
 
-</script>
-
-<!--
-<script>
-export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
-  computed: {
-    myComputedProperty() {
-      // perform logic based on a data property
-      return 'my result'
-    }
-  },
-  watch: {
-    count(newCount, oldCount) {
-      if (newCount == 20) alert('asdfasd')
-    }
-  },
-  mounted() {
-    console.log('mounted')
-  },
-  unmounted() {
-    console.log('unmounted')
-  },
-  directives: {
-    autofocus: {
-      mounted(el) {
-        el.focus()
-      }
-    }
-  }
+const increaseCounter = (amount, e) => {
+  counterData.count += amount
+  console.log({e})
 }
+
+const decreaseCounter = amount => {
+  counterData.count -= amount
+}
+
+onMounted(() => {
+  console.log('Do stuff related to Counter')
+})
+
 </script>
--->
 
 <style>
 .home {
   text-align: center;
   padding: 20px;
 }
+
 .btn, .counter {
   font-size: 40px;
   margin: 10px;
 }
+
 .edit {
   margin-top: 60px;
 }
