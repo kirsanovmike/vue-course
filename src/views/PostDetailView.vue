cd0206d3c13ac62922d23730101c0c7428bdf353
kirsanovmike <kirsanovmike@gmail.com>
2023-12-08 14:38:43 +0300

    smallfix

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -11,23 +11,25 @@
     <div>
       <button @click="goToFirstPost">Go to first post</button>
     </div>
-    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+    <p>
+      <RouterLink to="/posts">&lt; Back</RouterLink>
+    </p>
   </div>
 </template>
 
 <script setup>
-import { useRoute, useRouter } from 'vue-router'
+import {useRoute, useRouter} from 'vue-router'
 
 const route = useRoute()
 const router = useRouter()
 
 const showPostId = () => {
-  alert(`The ID of this post is: ${ route.params.id }`)
+  alert(`The ID of this post is: ${route.params.id}`)
 }
 
 const goHomeIn3Seconds = () => {
   setTimeout(() => {
-    router.push({ name: 'home' })
+    router.push({name: 'home'})
   }, 3000)
 }
 
