9c4a7f2e9b5ea858c831de343e04e80904df5790
kirsanovmike <kirsanovmike@gmail.com>
2024-04-12 15:54:18 +0300

    new stuff

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,5 @@
 "use strict";
 importScripts("sw-toolbox.js");
+toolbox.precache(["index.html", "main.css"]);
 toolbox.router.get("/icons/*", toolbox.cacheFirst);
 toolbox.router.get("/*", toolbox.networkFirst, {networkTimeoutSeconds: 5});
